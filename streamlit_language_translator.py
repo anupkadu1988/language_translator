f9563487daaec51153952e1d4f272648a345bdd0
anupkadu1988 <anupkadu1988@gmail.com>
2025-06-05 23:46:01 +0530

    streamlit language translator

---
+++ streamlit_language_translator.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+
+load_dotenv()
+
+## Build a model
+groq_api_key=os.getenv("GROQ_API_KEY")
+groq_model=ChatGroq(model="llama-3.3-70b-versatile", groq_api_key=groq_api_key)
+
+## Prompt
+gen_message="Translate given input in language {language}"
+prompt=ChatPromptTemplate.from_messages(
+    [("system", gen_message), ("user", "{query}")]
+)
+
+## parser
+parser=StrOutputParser()
+
+## chain
+chain=prompt|groq_model|parser
+
+st.title("language translator")
+
+target_language=st.text_input("Mention your target language here")
+query=st.text_input("Mention query to translate here")
+
+if st.button('translate'):
+    response=chain.invoke({"language":target_language,"query":query})
+    st.write(response)
+
